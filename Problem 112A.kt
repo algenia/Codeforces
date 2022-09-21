87f9e281143efcd8f2dd78b09e92a52e3512944a
algenia <52661627+algenia@users.noreply.github.com>
2022-09-21 23:01:36 +0200

    Update Problem 112A.kt

--- Problem 112A.kt
+++ Problem 112A.kt
@@ -2,25 +2,25 @@
 // Petya and Strings
 
 fun main() {
-    val line1 = readln().lowercase() // read first string from input, decapitalize
-    val line2 = readln().lowercase() // read second string from input, decapitalize
+    val line1 = readln().lowercase()
+    val line2 = readln().lowercase()
     var more = 0
     var less = 0
     var i = line1.length
     var j = 0
-    while (i > 0 && more == 0 && less == 0) { // go through strings elements
-        if (line1[j] > line2[j]) { // difference found, print result, exit loop
+    while (i > 0 && more == 0 && less == 0) {
+        if (line1[j] > line2[j]) {
             println("1")
             more++
         }
-        else if (line1[j] < line2[j]){ // difference found, print result, exit loop
+        else if (line1[j] < line2[j]){
             println("-1")
             less++
         }
         else {
             i--
             j++
-            if (i == 0) println("0") // end of loop, strings are the same, print result
+            if (i == 0) println("0")
         }
     }
 }
