0d41927a6df6b5cb570d3d12521c691e01418e1f
algenia <52661627+algenia@users.noreply.github.com>
2022-09-21 23:06:14 +0200

    Update Problem 50A.kt

--- Problem 50A.kt
+++ Problem 50A.kt
@@ -2,6 +2,6 @@
 // Domino piling
 
 fun main() {
-    val line = readln().split(" ") // read line from the input, save numbers only
-    println(line[0].toInt() * line[1].toInt() / 2) // print the result of div by 2
+    val line = readln().split(" ")
+    println(line[0].toInt() * line[1].toInt() / 2)
 }
