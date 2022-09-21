e8b4ed28e7a4d2ca8d9dc87bfcc66e9bf4855146
algenia <52661627+algenia@users.noreply.github.com>
2022-09-21 23:04:23 +0200

    Update Problem 282A.kt

--- Problem 282A.kt
+++ Problem 282A.kt
@@ -2,12 +2,12 @@
 // Bit++
 
 fun main() {
-    var n = readln().toInt() // read number of sentences from input
+    var n = readln().toInt()
     var x = 0
     while (n > 0) { // read n lines
         val next = readln()
-        if (next[1].compareTo('+') == 0) x++ // increase x if second character in line is +
-        else x-- // decrease x if second character in line is not +
+        if (next[1].compareTo('+') == 0) x++
+        else x--
         n--
     }
     println(x)
